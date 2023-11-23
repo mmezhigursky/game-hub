d06b7bcfb853e017cb3258c6bdd68377df0d7969
max <m.mezhigurskii@gmail.com>
2023-11-23 21:41:38 +0100

    add genre pannel

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,26 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderEndRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize={"lg"}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
