43c5705b17b4d2dca060ecc06ad746be69ad2365
max <m.mezhigurskii@gmail.com>
2023-11-24 15:20:31 +0100

    filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import apiClient from "../services/api-client";
-import { CanceledError } from 'axios';
+import {AxiosRequestConfig, CanceledError } from 'axios';
 
 
 export interface FetchResponse<T> {
@@ -8,7 +8,7 @@ export interface FetchResponse<T> {
     results: T[];
     }
 
-const useData = <T> (endpoint: string) => {
+const useData = <T> (endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false)
@@ -17,7 +17,7 @@ const useData = <T> (endpoint: string) => {
     const controller = new AbortController()
     setLoading(true)
     apiClient
-      .get<FetchResponse<T>>(endpoint, {signal: controller.signal})
+      .get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
       .then((res) => {setData(res.data.results);
       setLoading(false);})
       .catch((err) => { if (err instanceof CanceledError) return;
@@ -27,7 +27,7 @@ const useData = <T> (endpoint: string) => {
       return () => {
         controller.abort()
       }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, deps ? [...deps]: []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return { data, error, isLoading };
 };
